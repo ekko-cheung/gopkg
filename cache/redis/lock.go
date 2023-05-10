228fdfccb84ea07ab368effd882acae8c5962505
veerdone <13112433432@163.com>
2023-05-10 20:49:03 +0800

    perf: redis lock TryLock

--- cache/redis/lock.go
+++ cache/redis/lock.go
@@ -29,14 +29,38 @@ func NewLock(key string, ctx context.Context) *Lock {
 	}
 }
 
-func (l *Lock) Lock(val interface{}, exTime time.Duration) {
-	l.lock(val, exTime)
+func (l *Lock) Lock(val interface{}, exTime time.Duration) bool {
+	ok, err := l.lock(val, exTime)
+	if err != nil || !ok {
+		return false
+	}
+
+	go l.watchDog(exTime)
+	return true
 }
 
-func (l *Lock) TryLock(val interface{}, exTime time.Duration) bool {
-	ok, _ := l.lock(val, exTime)
-
-	return ok
+// TryLock try lock in waitTime, eg:
+//
+// 	ctx, cancel := context.WithCancel(ctx)
+//  l := NewLock(key, ctx)
+// 	defer cancel()
+//  if l.TryLock(val, time.Second, time.Second) {
+//    defer l.Unlock(val)
+//    // business
+//  }
+func (l *Lock) TryLock(val interface{}, waitTime, exTime time.Duration) bool {
+	ctx, cancel := context.WithTimeout(l.ctx, waitTime)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			return false
+		default:
+			if l.Lock(val, exTime) {
+				return true
+			}
+		}
+	}
 }
 
 func (l *Lock) lock(val interface{}, exTime time.Duration) (bool, error) {
@@ -46,6 +70,18 @@ func (l *Lock) lock(val interface{}, exTime time.Duration) (bool, error) {
 	}
 
 	return b, err
+}
+
+func (l *Lock) watchDog(exTime time.Duration) {
+	for {
+		select {
+		case <-l.ctx.Done():
+			return
+		default:
+			l.cli.Expire(l.ctx, l.key, exTime)
+			time.Sleep(exTime / 3)
+		}
+	}
 }
 
 func (l *Lock) UnLock(val interface{}) {
